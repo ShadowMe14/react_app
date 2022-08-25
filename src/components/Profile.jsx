f5adb1cfbf97ca541dd61c17b5d2f656d2515d7e
ShadowMe14 <lutarevich.alina@gmail.com>
2022-08-25 21:12:35 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <img src='https://wallpaperaccess.com/full/1131217.jpg'></img>
+    <div>ava</div>
+    <div>my posts
+      <div>new post</div>
+      <div>post 1</div>
+      <div>post 2</div>
+    </div>
+  </div>
+}
+
+export default Profile;
